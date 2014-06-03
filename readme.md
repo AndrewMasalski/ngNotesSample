11da9ba9385b4094ba3840343bd2026ac005e729
Andrei Masalski <amasalski@exadel.com>
2014-06-03 18:49:29 +0300

    Initial commit

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 ### Sample application on angularjs consuming WebAPI 2 web service.
+
+Uses ASP.NET Web API to get data from angular web application
---
+++ webapiSample/NotesService/Controllers/NotesController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http.Cors;
+using NotesServiceApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace NotesServiceApp.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class NotesController : ApiController
+    {
+        private readonly Note[] notes =
+        { 
+            new Note { Id = 1, Title = "Kill all humans", Description = "With deadly virus or something..." }, 
+            new Note { Id = 2, Title = "Ressurect all humans", Description = "Virus will do ok" }, 
+        };
+
+        public IEnumerable<Note> GetAllNotes()
+        {
+            return notes;
+        }
+
+        public IHttpActionResult GetNotes(int id)
+        {
+            var note = notes.FirstOrDefault(p => p.Id == id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            return Ok(note);
+        }
+    }
+}
